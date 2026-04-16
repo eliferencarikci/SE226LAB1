2b6fc4119b1101d872ab918e47f5b7af9bc05a96
Elif Eren Çarıkcı <carikcieliferen@gmail.com>
2026-04-16 13:45:49 +0300

    Add files via upload

---
+++ LAB6/data_package/analyzer.py
@@ -0,0 +1,8 @@
+def calculate_mean(num_list):
+    return sum(num_list) / len(num_list) if num_list else 0
+
+def find_maximum(num_list):
+    return max(num_list) if num_list else None
+
+def find_minimum(num_list):
+    return min(num_list) if num_list else None
